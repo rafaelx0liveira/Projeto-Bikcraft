0b19547e6cfcb24de269813d3bb1a1970c07b96e
R4f43lA94r3c1d0 <rafaelaparecido.oliveirasilva@gmail.com>
2022-02-05 21:05:17 -0300

    Parâmetros URL

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
-const links = document.querySelectorAll('.header-menu a');
+/****************************
+        ATIVAR LINKS
+****************************/
 
-function ativarLink(link){
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
   const url = location.href;
   const href = link.href;
 
-  if(url.includes(href)){
+  if (url.includes(href)) {
     link.classList.add("ativo");
   }
 
-
   console.log(url);
   console.log(href);
-
 }
 
-
 links.forEach(ativarLink);
+
+/***********************************
+        ATIVAR ITENS ORÇAMENTO
+************************************/
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+
+  if(elemento)
+  {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
