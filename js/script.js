526901b9a870be5eff4b362f02f06ca84baa77db
R4f43lA94r3c1d0 <rafaelaparecido.oliveirasilva@gmail.com>
2022-02-05 22:18:04 -0300

    Galeria Imagens

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 
 /*********************************
-        PRGUNTAS FREQUENTES
+        PERGUNTAS FREQUENTES
 **********************************/
 const perguntas = document.querySelectorAll(".perguntas button");
 
@@ -55,3 +55,23 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+/***********************************
+        GALERIA DE BICICLETAS
+************************************/
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
